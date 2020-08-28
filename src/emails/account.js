481e2e4a4af49bf4f810a186aafddca59ba19b39
cadaci0 <roadtodiploma@roadtodiplomas-MacBook-Pro.local>
2020-08-28 18:34:11 +0200

    emailadd. has been changed

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,11 +1,11 @@
 const sgMail = require('@sendgrid/mail')
 
-sgMail.setApiKey('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')
+sgMail.setApiKey('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')
 
 const sendWelcomeEmail = (email, username) => {
     sgMail.send({
         to: email,
-        from: 'welcome@flashclix.at',
+        from: 'welc0me@flashclix.at',
         subject: 'Thanks for joining in!',
         text: `Welcome to flashclix app, ${username}. Let me know how you get along with the flashclix app.`
     })
@@ -24,7 +24,7 @@ const sendCancelationEmail = (email, username) => {
 const forgotPasswordEmail = (email, username, password) => {
     sgMail.send({
         to: email,
-        from: 'forgotpassword@flashclix.at',
+        from: 'passwordreset@flashclix.at',
         subject: 'Reset password request!',
         text: `Hello ${username}. A request has been received that you forgot your password. 
         Your new password is ${password}. Now you can sign in with your new password.`
